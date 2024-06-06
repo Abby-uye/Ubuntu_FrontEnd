5e62950d25bceb8be26ee75b5ba0810831a3ae88
Ojo Oluwatobi <ojot630@gmail.com>
2024-06-06 05:01:52 +0100

    Worked on The like Post functionalities on 06/06/2024

--- src/features/ViewAllPost/index.jsx
+++ src/features/ViewAllPost/index.jsx
@@ -2,12 +2,28 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import styles from "./index.module.css"
 import { BACKEND_POST_BASE_URL } from "../../ApiUtils";
+import { FcLike } from "react-icons/fc";
+import { FcDislike } from "react-icons/fc";
+import {jwtDecode} from "jwt-decode"
+import { useNavigate } from "react-router-dom";
 
 const ViewAllPost = () => {
+    const navigate = useNavigate();
     const [posts, setPosts] = useState([]);
     const [serverError, setServerError] = useState("");
+    const [userId, setUserId] = useState("");
 
     useEffect(() => {
+        try{
+            const token = localStorage.getItem("token");
+            if(token){
+            const decodedToken = jwtDecode(token);
+            setUserId(decodedToken.sender_email);
+            }else navigate("/home");
+        }catch(error){
+            console.log(error);
+        }
+
         const handleGetAllPost = async () => {
             try {
                 const response = await axios.get("http://localhost:8080/ubuntu/post/all_post");
@@ -26,24 +42,65 @@ const ViewAllPost = () => {
 
     const handleLikeBtn = async(postId) => {
         try{
-           const response = await axios.post(BACKEND_POST_BASE_URL+"/like/"+postId);
+            const payload = {
+                userId: userId,
+                postId: postId
+            }
+           const response = await axios.post(BACKEND_POST_BASE_URL+"/like", payload);
+           if(response.status === 202){
+            setPosts((prevPosts) =>
+                prevPosts.map((post) =>
+                    post.id === postId
+                        ? {
+                              ...post,
+                              likes: {
+                                  ...post.likes,
+                                  [userId]: !post.likes[userId]
+                              }
+                          }
+                        : post
+                )
+           )}
         }catch(error){
             console.log(error);
         }
     }
 
+   
 
+    const getNumberOfLikes = (likes) => {
+        likes = new Map(Object.entries(likes));
+        let numberOfLike = 0;
+        for(var like of likes.values()){
+            if(like){
+                numberOfLike += 1;
+            }
+        }
+        return numberOfLike;
+    }
+
+    const userHasLikePost = (likes) => {
+        const objToMap = new Map(Object.entries(likes));
+        return objToMap.has(userId);
+    }
+
+    const userLikePost = (likes) => {
+        const objToMap = new Map(Object.entries(likes));
+        return objToMap.get(userId);
+    }
 
     return (
         <div className={styles.viewAllPost} >
-            {posts.length > 0 ? (
+            {posts && posts.length > 0 ? (
                 posts.map((post, index) => (
                     <div key={index}>
                         <h2>{post.title}</h2>
                         <p>{post.body}</p>
                         {post.image && <img src={post.image} alt="event"/>}
                         <div className={styles.likeAndComment}>
-                           <button onClick={(event) => handleLikeBtn(post.id)} className={styles.theButton}>Like{" " + post.numberOfLikes}</button>
+                         <button onClick={(event) => handleLikeBtn(post.id)} className={styles.theButton}>
+                            {post.likes && userHasLikePost(post.likes) && userLikePost(post.likes) ? <FcLike/> : <FcDislike/>} {getNumberOfLikes(post.likes)}
+                        </button>
                             <button className={styles.theButton}>Comment</button>
                         </div>
                     </div>
